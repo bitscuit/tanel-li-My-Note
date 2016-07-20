90bfa0613c05d7354e88c68240f8526e66268d2d
bitscuit <li_zhao_bin@hotmail.com>
2016-07-20 11:57:08 -0600

    set up content provider for notes database - constants done, onCreate done, manifest done

---
+++ app/src/main/java/com/beauty/comp_eng/mynote/NoteProvider.java
@@ -0,0 +1,75 @@
+package com.beauty.comp_eng.mynote;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+// this class is the content provider
+// creates a standardized mechanism to get the app's data
+public class NoteProvider extends ContentProvider {
+
+    // AUTHORITY is a globally unique string that identifies the content provider
+    // to the Android framework.
+    private static final String AUTHORITY = "com.beauty.comp_eng.mynote.noteprovider";
+
+    // represents the entire data set. Only one table, so "notes"
+    private static final String BASE_PATH = "notes";
+
+    // uniform resource identifier to identify the content provider
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
+
+    // constants to identify requested operation
+    private static final int NOTES = 1;             // give data
+    private static final int NOTES_ID = 2;          // give a single row in database
+
+    // Uri matcher to parse a URI and then determine which operation has been requested
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // static initializer for UriMatcher
+    static {
+        uriMatcher.addURI(AUTHORITY, BASE_PATH, NOTES);
+        // "#" is a wildcard
+        uriMatcher.addURI(AUTHORITY, BASE_PATH + "/#", NOTES_ID);
+    }
+
+    private SQLiteDatabase db;
+
+    @Override
+    public boolean onCreate() {
+        DBOpenHelper helper = new DBOpenHelper(getContext());
+        db = helper.getWritableDatabase();
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
